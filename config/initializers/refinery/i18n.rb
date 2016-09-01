5b12ea7f6de814e9da31e32ace0e15708b5f8cb2
catcosmo <astrangethought@hotmail.com>
2016-09-01 13:59:38 +0200

    Initial commit

---
+++ config/initializers/refinery/i18n.rb
@@ -0,0 +1,13 @@
+# encoding: utf-8
+
+Refinery::I18n.configure do |config|
+ config.default_locale = :de
+
+  config.current_locale = :de
+
+  config.default_frontend_locale = :de
+
+  config.frontend_locales = [:de, :en, :es]
+
+  config.locales = {:de=>"Deutsch", :en=>"English", :es=>"Español"}
+end
